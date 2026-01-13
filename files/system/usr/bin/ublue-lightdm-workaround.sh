e41e58adf88b18a8e0d7abd692a0316c27c5afbb
Denis Garaev <garaevdi@yandex.ru>
2026-01-13 04:51:08 +0300

    fix: Try to fix lightdm

---
+++ files/system/usr/bin/ublue-lightdm-workaround.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d /var/cache/lightdm ]; then echo "creating /var/cache/lightdm";mkdir /var/cache/lightdm; fi
+
+if [ ! -d /var/log/lightdm ]; then echo "creating /var/log/lightdm";mkdir /var/log/lightdm; fi
+if [ ! -d /var/lib/lightdm ]; then echo "creating /var/lib/lightdm";mkdir /var/lib/lightdm; fi
+if [ ! -d /var/lib/lightdm-data ]; then echo "creating /var/lib/lightdm-data";mkdir /var/lib/lightdm-data; fi
+
+getent group lightdm >/dev/null || groupadd -r lightdm
+echo "created lightdm group"
+getent passwd lightdm >/dev/null || useradd -g lightdm -M -d /var/lib/lightdm -s /sbin/nologin -r lightdm
+echo "added lightdm user"
+systemctl set-default graphical.target
